4d90a41879128f5b66f2bdcb115560fc8d5557d2
Evgeny Kurnevsky <kurnevsky@gmail.com>
2021-03-22 12:21:13 +0300

    Add cachix binary cache.

--- configuration.nix
+++ configuration.nix
@@ -27,6 +27,9 @@
     package = pkgs.nixFlakes;
     autoOptimiseStore = true;
     extraOptions = "experimental-features = nix-command flakes";
+    binaryCaches = [ "https://cachix.cachix.org" ];
+    binaryCachePublicKeys =
+      [ "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM=" ];
   };
 
   # networking.hostName = "nixos";
