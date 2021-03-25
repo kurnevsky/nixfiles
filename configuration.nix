682dbe0f9f001bbaf087108f8621c6e0609986ba
Evgeny Kurnevsky <kurnevsky@gmail.com>
2021-03-25 23:19:48 +0300

    Configure zsap.

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,16 @@
     v4l2loopback
   ];
   boot.kernelParams = [ "zswap.enabled=1" ];
+  boot.initrd.kernelModules = [ "lz4" "z3fold" ];
+  systemd.services.zswap-configure = {
+    description = "Configure zswap";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+      echo lz4 > /sys/module/zswap/parameters/compressor
+      echo z3fold > /sys/module/zswap/parameters/zpool
+    '';
+  };
 
   fileSystems."/".options = [ "noatime" "nodiratime" "compress=zstd:3" ];
 
