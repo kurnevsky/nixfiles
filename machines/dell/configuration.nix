8c6fb1ae732f794484c6dc289bba259365506443
Evgeny Kurnevsky <kurnevsky@gmail.com>
2021-08-05 00:15:02 +0300

    Fix sound glitches.

--- machines/dell/configuration.nix
+++ machines/dell/configuration.nix
@@ -16,6 +16,8 @@
       "radeon.si_support=0"
       "amdgpu.cik_support=1"
       "amdgpu.si_support=1"
+      # Fixes sound glitches
+      "intel_iommu=igfx_off"
     ];
     extraModulePackages = with config.boot.kernelPackages; [
       acpi_call
