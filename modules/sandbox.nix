317261dbc9239f2a9ed29e07298099c7d80c2bbf
Evgeny Kurnevsky <kurnevsky@gmail.com>
2021-08-05 22:36:42 +0300

    Allow certificates for wine.

--- modules/sandbox.nix
+++ modules/sandbox.nix
@@ -371,6 +371,7 @@ let
         "devices"
       ];
       x11 = true;
+      etcs = [ "ssl/certs/ca-certificates.crt" ];
       pams = [ "bus" "pulse" ];
       localtime = true;
       unsetenvs = [ "MAIL" ];
