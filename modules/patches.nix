076d9dd16322f117662b8e210a5429552fe34dfe
Evgeny Kurnevsky <kurnevsky@gmail.com>
2021-08-22 13:14:36 +0300

    Bump tor-browser version.

--- modules/patches.nix
+++ modules/patches.nix
@@ -3,7 +3,19 @@
 let
   patchesDrv = pkgs.applyPatches {
     src = pkgs.path;
-    patches = [ ];
+    patches = [
+      (pkgs.fetchpatch {
+        url =
+          "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/135032.diff";
+        sha256 = "sha256-KfBNFOVEhVdIYqYw4QU3CWRuH1ZxahyXlsrq+MOo4tE=";
+      })
+    ];
   };
   patchedPkgs = (import patchesDrv { system = "x86_64-linux"; });
-in { }
+in {
+  nixpkgs.overlays = [
+    (self: super: {
+      tor-browser-bundle-bin = patchedPkgs.tor-browser-bundle-bin;
+    })
+  ];
+}
