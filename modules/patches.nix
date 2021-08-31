d10beff5e4e79da8c2a194a6a2b54bba0265e5f4
Evgeny Kurnevsky <kurnevsky@gmail.com>
2021-08-31 13:42:34 +0300

    Update packages.

--- modules/patches.nix
+++ modules/patches.nix
@@ -3,19 +3,7 @@
 let
   patchesDrv = pkgs.applyPatches {
     src = pkgs.path;
-    patches = [
-      (pkgs.fetchpatch {
-        url =
-          "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/135032.diff";
-        sha256 = "sha256-KfBNFOVEhVdIYqYw4QU3CWRuH1ZxahyXlsrq+MOo4tE=";
-      })
-    ];
+    patches = [ ];
   };
   patchedPkgs = (import patchesDrv { system = "x86_64-linux"; });
-in {
-  nixpkgs.overlays = [
-    (self: super: {
-      tor-browser-bundle-bin = patchedPkgs.tor-browser-bundle-bin;
-    })
-  ];
-}
+in { }
