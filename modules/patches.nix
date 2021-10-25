ee24c58632446ac48fc66d9dd9397085fc8b6f5e
Evgeny Kurnevsky <kurnevsky@gmail.com>
2021-10-25 19:02:39 +0300

    Update tor-browser.

--- modules/patches.nix
+++ modules/patches.nix
@@ -3,7 +3,19 @@
 let
   patchesDrv = pkgs.applyPatches {
     src = pkgs.path;
-    patches = [ ];
+    patches = [
+      (pkgs.fetchpatch {
+        url =
+          "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/140966.diff";
+        sha256 = "sha256-Ydqvbn4q+oakVRPrQkavXRJMuIGLluO38nNTCTgumqM=";
+      })
+    ];
   };
   patchedPkgs = (import patchesDrv { system = "x86_64-linux"; });
-in { }
+in {
+  nixpkgs.overlays = [
+    (self: super: {
+      tor-browser-bundle-bin = patchedPkgs.tor-browser-bundle-bin;
+    })
+  ];
+}
