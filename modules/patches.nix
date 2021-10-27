e85ba31356024907e83484eb3482355a8042ce70
Evgeny Kurnevsky <kurnevsky@gmail.com>
2021-10-27 22:32:14 +0300

    Add pidgin patch for certificates.

--- modules/patches.nix
+++ modules/patches.nix
@@ -9,6 +9,11 @@ let
           "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/140966.diff";
         sha256 = "sha256-QXnQYoMiy536UjAWuLSC7ivJj5AQVU8rveevYTC091o=";
       })
+      (pkgs.fetchpatch {
+        url =
+          "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/143127.diff";
+        sha256 = "sha256-Q4AWXIrqhSFymVzZ+1EaJRBVAQDCyIHuQVLIzcqNtQE=";
+      })
     ];
   };
   patchedPkgs = (import patchesDrv { system = "x86_64-linux"; });
@@ -16,6 +21,7 @@ in {
   nixpkgs.overlays = [
     (self: super: {
       tor-browser-bundle-bin = patchedPkgs.tor-browser-bundle-bin;
+      pidgin = patchedPkgs.pidgin;
     })
   ];
 }
