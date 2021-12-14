16f85b166e1e5389ca60db24ee85746f7fc80ff0
Evgeny Kurnevsky <kurnevsky@gmail.com>
2021-12-14 07:49:01 +0300

    Add patch for toxcore to fix vulnerability.

--- modules/patches.nix
+++ modules/patches.nix
@@ -3,7 +3,16 @@
 let
   patchesDrv = pkgs.applyPatches {
     src = pkgs.path;
-    patches = [ ];
+    patches = [
+      (pkgs.fetchpatch {
+        url =
+          "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/150614.diff";
+        sha256 = "sha256-6NICgOIkxPTbToKeIAcTAQVhHe76KEqj8dx7I7G8ulA=";
+      })
+    ];
   };
   patchedPkgs = (import patchesDrv { system = "x86_64-linux"; });
-in { nixpkgs.overlays = [ (self: super: { }) ]; }
+in {
+  nixpkgs.overlays =
+    [ (self: super: { libtoxcore = patchedPkgs.libtoxcore; }) ];
+}
