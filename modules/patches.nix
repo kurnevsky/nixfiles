37e64197049b96f262d325b661be2acf32fe224c
Evgeny Kurnevsky <kurnevsky@gmail.com>
2021-12-28 20:42:50 +0300

    Bump.

--- modules/patches.nix
+++ modules/patches.nix
@@ -3,16 +3,7 @@
 let
   patchesDrv = pkgs.applyPatches {
     src = pkgs.path;
-    patches = [
-      (pkgs.fetchpatch {
-        url =
-          "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/150614.diff";
-        sha256 = "sha256-6NICgOIkxPTbToKeIAcTAQVhHe76KEqj8dx7I7G8ulA=";
-      })
-    ];
+    patches = [ ];
   };
   patchedPkgs = (import patchesDrv { system = "x86_64-linux"; });
-in {
-  nixpkgs.overlays =
-    [ (self: super: { libtoxcore = patchedPkgs.libtoxcore; }) ];
-}
+in { nixpkgs.overlays = [ (self: super: { }) ]; }
