783144ea42ee5d88ac7325bf7aa0ccce83f996bf
Evgeny Kurnevsky <kurnevsky@gmail.com>
2022-02-27 23:30:51 +0300

    Move some programs configuration to common.

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -273,66 +273,7 @@
   programs = {
     dconf.enable = true;
     gnupg.agent.enable = true;
-    bash.interactiveShellInit = ''
-      # Set cursor type to steady bar
-      echo -e -n "\x1b[\x36 q"
-    '';
-    zsh = {
-      enable = true;
-      enableCompletion = true;
-      # will be enabled in interactiveShellInit differently
-      enableGlobalCompInit = false;
-      autosuggestions = {
-        enable = true;
-        extraConfig = { ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE = "10"; };
-      };
-      syntaxHighlighting.enable = true;
-      # Override default prompt as it doesn't work good in Midnigt Commander
-      promptInit = "PROMPT='[%n@%m %~]$ '";
-      histSize = 25000;
-      histFile = "~/.histfile";
-      setOptions = [
-        # Remove all duplicates of current command from history, add current to end
-        "hist_ignore_all_dups"
-        # Don't save any commands beginning with space
-        "hist_ignore_space"
-        # Enable extended globs to interpret things like rm ^(file|file2)
-        "extended_glob"
-        # Don't beep even if zsh don't like something
-        "no_beep"
-        # Change directory even if user forgot to put 'cd' command in front, but entered path is valid
-        "auto_cd"
-        # If possible, correct commands
-        "correct"
-        # Append their history list to the history file, rather than replace it
-        "inc_append_history"
-        # If a pattern for filename generation has no matches, print an error, instead of leaving it unchanged in the argument list
-        "nomatch"
-        # Report the status of background jobs immediately, rather than waiting until just before printing a prompt
-        "notify"
-        # Allow parameter expansion, command substitution and arithmetic expansion for prompt string
-        "prompt_subst"
-        # Remove any right prompt from display when accepting a command line
-        "transient_rprompt"
-        # File completion after =
-        "magic_equal_subst"
-        # Apply globbing to hidden files
-        "glob_dots"
-        # Use OS file locking
-        "hist_fcntl_lock"
-      ];
-      shellAliases = {
-        zmv = "noglob zmv -W";
-        calc = "noglob zcalc -e";
-      };
-      interactiveShellInit = builtins.readFile ./interactive-init.zsh;
-    };
     adb.enable = true;
-    less = {
-      enable = true;
-      lessopen = "| ${pkgs.sourceHighlight}/bin/src-hilite-lesspipe.sh %s";
-      envVariables.LESS = " -R ";
-    };
   };
 
   gtk.iconCache.enable = true;
