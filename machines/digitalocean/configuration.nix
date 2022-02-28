648ca97830340f3224873c690b45678c768ae51c
Evgeny Kurnevsky <kurnevsky@gmail.com>
2022-02-28 08:58:01 +0300

    Configure tt-rss.

--- machines/digitalocean/configuration.nix
+++ machines/digitalocean/configuration.nix
@@ -2,4 +2,24 @@
   boot.cleanTmpDir = true;
 
   networking.hostName = "digitalocean";
+
+  services = {
+    postgresql = {
+      enable = true;
+      ensureDatabases = [ "ttrss" ];
+      ensureUsers = [{
+        name = "ttrss";
+        ensurePermissions = { "DATABASE \"ttrss\"" = "ALL PRIVILEGES"; };
+      }];
+    };
+    tt-rss = {
+      enable = true;
+      database = {
+        name = "ttrss";
+        user = "ttrss";
+      };
+      sessionCookieLifetime = 2592000;
+      selfUrlPath = "https://kurnevsky.net/tt-rss/";
+    };
+  };
 }
