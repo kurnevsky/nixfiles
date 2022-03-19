5117b6cf32e625d776a824e0acf71a6272c4c05d
Evgeny Kurnevsky <kurnevsky@gmail.com>
2022-03-19 12:08:10 +0300

    Update yggdrasil peers.

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -274,7 +274,8 @@
       enable = true;
       config = {
         Peers = [
-          "tls://yggby.ddns.net:7992"
+          "tls://yggno.de:18227"
+          "tls://box.paulll.cc:13338"
           "tls://54.37.137.221:11129"
           "tls://pl1.servers.devices.cwinfo.net:11129"
           "tls://193.111.114.28:1443"
