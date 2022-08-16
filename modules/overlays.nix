f5da095d21b0069a7bfe80fdc0769d97d8a01b4e
Evgeny Kurnevsky <kurnevsky@gmail.com>
2022-08-16 17:59:13 +0300

    Pin globalprotect-openconnect to 1.4.7.
    1.4.8 doesn't work.

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -113,5 +113,17 @@
         ];
       });
     })
+    (self: super: {
+      globalprotect-openconnect = super.globalprotect-openconnect.overrideAttrs (old: rec {
+        version = "1.4.7";
+        src = super.fetchFromGitHub {
+          owner = "yuezk";
+          repo = "GlobalProtect-openconnect";
+          fetchSubmodules = true;
+          rev = "v${version}";
+          sha256 = "sha256-MNH6zizPX3tcFsEPC5w0lr48KlV578kYe+f5v8Qc5FY=";
+        };
+      });
+    })
   ];
 }
---
+++ modules/pan-globalprotect-okta.nix
@@ -0,0 +1,24 @@
+{ fetchFromGitHub, python3, python3Packages }:
+
+python3Packages.buildPythonPackage rec {
+  pname = "pan-globalprotect-okta";
+  version = "1.0.0";
+
+  format = "other";
+
+  src = fetchFromGitHub {
+    owner = "arthepsy";
+    repo = "pan-globalprotect-okta";
+    rev = "da771a46c6e21b15c1fe4a0325222097028cffc5";
+    sha256 = "";
+  };
+
+  propagatedBuildInputs = with python3Packages; [ requests lxml pyotp ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src/gp-okta.py $out/bin/gp-okta
+  '';
+
+  doCheck = false;
+}
