b261111c7638c09a92b21ed1f7f82ea0064bdb5f
Evgeny Kurnevsky <kurnevsky@gmail.com>
2022-09-13 00:16:17 +0300

    Bump shapeshifter.

--- modules/shapeshifter-dispatcher.nix
+++ modules/shapeshifter-dispatcher.nix
@@ -1,17 +1,25 @@
-{ lib, buildGoModule, fetchFromGitHub, ... }:
+{ lib, buildGoModule, fetchFromGitHub, fetchpatch, ... }:
 
 buildGoModule rec {
   pname = "shapeshifter-dispatcher";
-  version = "2.2.1";
+  version = "3.0.1";
 
   src = fetchFromGitHub {
     owner = "OperatorFoundation";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-KSQAOoPH78jmdyZY6cDXXGyQRUeItAnMWyOk99GTJAU=";
+    sha256 = "sha256-QhPlg0Jh1V9Zea5uo6lUqj/8yhNW/i9m4muaFKn8IZs=";
   };
 
-  vendorSha256 = "sha256-lYD63wScqCZaidDCWVANMqL2ciStKaiGLMb0AuU7rjQ=";
+  patches = [
+    (fetchpatch {
+      url =
+        "https://github.com/OperatorFoundation/shapeshifter-dispatcher/commit/b936dc8004db276a5b639bbe6377895284e8d8c9.patch";
+      sha256 = "sha256-4fJFdY4xYN1ZBOseDidOieGiOpFAabZRyM8icBpkITI=";
+    })
+  ];
+
+  vendorSha256 = "sha256-D1Og94NKf7PG+BJ3ieaZsKzEDo/i6Bx+FjNnAT47g+k=";
 
   doCheck = false;
 
