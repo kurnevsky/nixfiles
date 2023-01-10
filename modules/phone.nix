c5279ef5f6270e28373cab90625f3b36138167d6
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-01-10 12:14:01 +0300

    Fix login on phone.

--- modules/phone.nix
+++ modules/phone.nix
@@ -1,8 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
+  users = {
+    mutableUsers = lib.mkForce true;
+    users.kurnevsky = {
+      passwordFile = lib.mkForce null;
+      password = "1234";
+    };
+  };
+
   services.xserver = {
     enable = true;
     desktopManager.plasma5.mobile.enable = true;
+    displayManager = {
+      sddm.enable = true;
+      defaultSession = "plasma-mobile";
+    };
   };
 }
