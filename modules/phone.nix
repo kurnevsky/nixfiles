cbe4f4be8a3162fa05ff207a5d360bf39d91cb42
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-01-15 12:00:04 +0300

    Start needed services.

--- modules/phone.nix
+++ modules/phone.nix
@@ -9,12 +9,40 @@
     };
   };
 
-  services.xserver = {
-    enable = true;
-    desktopManager.plasma5.mobile.enable = true;
-    displayManager = {
-      sddm.enable = true;
-      defaultSession = "plasma-mobile";
+  mobile.beautification.splash = true;
+
+  networking = {
+    useDHCP = false;
+    useNetworkd = true;
+    networkmanager = {
+      enable = true;
+      unmanaged = [ "rndis0" "usb0" ];
+    };
+  };
+
+  hardware = {
+    bluetooth.enable = true;
+    pulseaudio.enable = lib.mkForce false;
+    sensor.iio.enable = true;
+  };
+
+  services = {
+    pipewire = {
+      enable = true;
+      systemWide = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+    };
+    xserver = {
+      enable = true;
+      desktopManager.plasma5.mobile.enable = true;
+      displayManager = {
+        sddm.enable = true;
+        defaultSession = "plasma-mobile";
+      };
     };
   };
 }
