a7b3bc90a271963cdd70520c9ebd058f80383fa7
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-01-15 13:16:57 +0300

    Remove X11 modules.
    I don't use X11 anymore.

--- modules/xcb-client-id.nix
+++
@@ -1,9 +0,0 @@
-{ stdenv, xorg }:
-
-stdenv.mkDerivation {
-  name = "xcb-client-id";
-  src = ./xcb-client-id;
-  buildInputs = [ xorg.libxcb ];
-  buildPhase = "gcc -lxcb -lxcb-res -o xcb-client-id main.c";
-  installPhase = "mkdir -p $out/bin && cp xcb-client-id $out/bin";
-}
