6f84f88391b3a60844d05408fa8ff3ac35162564
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-03-07 22:10:04 +0300

    Use btrfs for rootfs.

--- machines/phone/configuration.nix
+++ machines/phone/configuration.nix
@@ -1,6 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
-{
+let
+  uuid = "e3b552a3-be28-4d1d-9016-e1b9a8699464";
+  label = "root";
+in {
+  mobile.generatedFilesystems.rootfs = lib.mkDefault {
+    type = lib.mkForce "btrfs";
+    label = lib.mkForce label;
+    id = lib.mkForce uuid;
+  };
+
+  fileSystems."/" = {
+    fsType = "btrfs";
+    options = [ "noatime" "nodiratime" "compress=zstd:3" ];
+    device = "/dev/disk/by-uuid/${uuid}";
+  };
+
   system.stateVersion = "22.11";
 
   home-manager.users = {
