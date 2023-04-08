4bc956a631401014f711d928fdc69ac9a3a662b4
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-04-08 23:58:42 +0300

    Less duplication.

--- modules/fzf-native.nix
+++ modules/fzf-native.nix
@@ -1,26 +1,28 @@
 { fetchFromGitHub, melpaBuild, writeText, cmake }:
 
-let rev = "ba06f5149e23c57510de2e67d5c46aee96356dba";
-in melpaBuild {
+let
+  rev = "ba06f5149e23c57510de2e67d5c46aee96356dba";
+  owner = "dangduc";
   pname = "fzf-native";
   version = "1";
+in melpaBuild {
+  inherit pname version;
 
   commit = rev;
 
   src = fetchFromGitHub {
-    owner = "dangduc";
-    repo = "fzf-native";
-    inherit rev;
+    inherit owner rev;
+    repo = pname;
     sha256 = "sha256-nRQTfkse+9icwJ4DEleiGbeIHEGqAMw97qk4jukdneE=";
   };
 
   nativeBuildInputs = [ cmake ];
 
   postInstall = ''
-    install -m=755 -D source/bin/Linux/fzf-native-module.so $out/share/emacs/site-lisp/elpa/fzf-native-1/bin/Linux/fzf-native-module.so
+    install -m=755 -D source/bin/Linux/fzf-native-module.so $out/share/emacs/site-lisp/elpa/${pname}-${version}/bin/Linux/fzf-native-module.so
   '';
 
   recipe = writeText "recipe" ''
-    (fzf-native :repo "dangduc/fzf-native" :fetcher github)
+    (${pname} :repo "${owner}/${pname}" :fetcher github)
   '';
 }
