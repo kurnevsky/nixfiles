46ba68a207bcf09b25bddaee9682dc091604b114
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-04-26 16:34:49 +0300

    Bump.

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -444,7 +444,8 @@
       enable = true;
       setSocketVariable = true;
     };
-    virtualbox.host.enable = true;
+    # TODO: broken
+    # virtualbox.host.enable = true;
   };
 
   hardware = {
