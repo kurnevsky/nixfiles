1fc68a3f6f38e876d3bd5c900168a55ebf184cac
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-05-18 11:29:27 +0300

    Add local trusted keys.

--- modules/common.nix
+++ modules/common.nix
@@ -18,6 +18,10 @@ in {
       trusted-public-keys = [
         "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        # local keys
+        "pc:NVXsu6EBpoB+a+W2ZTkpTWD9Vj5nJtjYsAfe8zTzDKE="
+        "evo:aBUuIOLObUPUBWBIW0XGyMZW2Wor5z3ZNtaRynZ35UY="
+        "digitalocean:jFnutzjWmSNY5q/frkSkijlCh8GfdNa1Mpm5Y0N15sQ="
       ];
     };
     extraOptions = ''
