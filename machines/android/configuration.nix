c0c50008e7b35cdc4b921ccce0b441892537c808
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-07-12 11:27:53 +0300

    Configure android nix.

--- machines/android/configuration.nix
+++ machines/android/configuration.nix
@@ -5,6 +5,24 @@ let
   emacsWithPackages = emacsPkgs.callPackage ../../modules/emacs-package.nix { }
     emacsPkgs.emacs-unstable-nox;
 in {
+  nix = {
+    substituters =
+      [ "https://cachix.cachix.org" "https://nix-community.cachix.org" ];
+    trustedPublicKeys = [
+      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      # local keys
+      "pc:NVXsu6EBpoB+a+W2ZTkpTWD9Vj5nJtjYsAfe8zTzDKE="
+      "evo:aBUuIOLObUPUBWBIW0XGyMZW2Wor5z3ZNtaRynZ35UY="
+      "digitalocean:jFnutzjWmSNY5q/frkSkijlCh8GfdNa1Mpm5Y0N15sQ="
+    ];
+    extraOptions = ''
+      experimental-features = nix-command flakes
+      keep-outputs = true
+      keep-derivations = true
+    '';
+  };
+
   environment.packages = with pkgs; [
     git
     lsof
