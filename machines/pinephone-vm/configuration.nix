adbaa8054a58feaa983ac156b200c166bf770645
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-07-16 12:11:33 +0300

    Move disk configuration to machines.

--- machines/pinephone-vm/configuration.nix
+++ machines/pinephone-vm/configuration.nix
@@ -1,7 +1,22 @@
 { ... }:
 
-{
-  mobile.boot.stage-1.kernel.additionalModules = [ "btrfs" ];
+let uuid = "e3b552a3-be28-4d1d-9016-e1b9a8699464";
+in {
+  mobile.generatedFilesystems.rootfs = lib.mkDefault {
+    # image builder uses `mkfs.btrfs --rootdir` which retains file owners
+    # this breaks nix daemon because /nix should be owned by root
+    # type = lib.mkForce "btrfs";
+    label = lib.mkForce "root";
+    id = lib.mkForce uuid;
+  };
+
+  fileSystems."/" = {
+    # fsType = "btrfs";
+    # options = [ "noatime" "nodiratime" "compress=zstd:3" ];
+    device = "/dev/disk/by-uuid/${uuid}";
+  };
+
+  # mobile.boot.stage-1.kernel.additionalModules = [ "btrfs" ];
 
   system.stateVersion = "23.05";
 
--- modules/pinephone.nix
+++ modules/pinephone.nix
@@ -1,21 +1,6 @@
 { pkgs, lib, ... }:
 
-let uuid = "e3b552a3-be28-4d1d-9016-e1b9a8699464";
-in {
-  mobile.generatedFilesystems.rootfs = lib.mkDefault {
-    # image builder uses `mkfs.btrfs --rootdir` which retains file owners
-    # this breaks nix daemon because /nix should be owned by root
-    # type = lib.mkForce "btrfs";
-    label = lib.mkForce "root";
-    id = lib.mkForce uuid;
-  };
-
-  fileSystems."/" = {
-    # fsType = "btrfs";
-    # options = [ "noatime" "nodiratime" "compress=zstd:3" ];
-    device = "/dev/disk/by-uuid/${uuid}";
-  };
-
+{
   # breaks encryption
   mobile.quirks.supportsStage-0 = lib.mkForce false;
 
