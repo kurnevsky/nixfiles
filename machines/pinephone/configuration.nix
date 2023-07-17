c13f1b0559605706a3a88b681a2377e80690a719
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-07-17 07:48:16 +0300

    Fix flashlight on pinephone.

--- machines/pinephone/configuration.nix
+++ machines/pinephone/configuration.nix
@@ -1,5 +1,11 @@
+{ pkgs, ... }:
+
 {
   networking.hostName = "pinephone";
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="leds", ACTION=="add", RUN+="${pkgs.coreutils}/bin/chgrp -R users /sys%p", RUN+="${pkgs.coreutils}/bin/chmod -R g=u /sys%p"
+  '';
 
   system.stateVersion = "23.05";
 
