a413feba751650580ce12b5033afb9202b2624d7
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-07-19 21:25:28 +0300

    Compile emacs config in a derivation.
    This way it can be shared in a cache. Also it's safer since onChange
    option doesn't execute script when script's dependency is changed.

--- modules/emacs/home.nix
+++ modules/emacs/home.nix
@@ -2,18 +2,58 @@ emacsPackage:
 
 { pkgs, ... }:
 
-{
-  home.file.".config/emacs/init.el" = {
-    source = ./init.el;
-    onChange = ''
-      mkdir -p ~/roam
-      export PATH=${pkgs.git}/bin:${pkgs.agda}/bin:"$PATH"
-      export EMACSLOADPATH=${pkgs.mu}/share/emacs/site-lisp/mu4e:
-      export EMACSNATIVELOADPATH=${pkgs.mu}/share/emacs/native-lisp:
-      rm -fv ~/.config/emacs/init.elc
-      ${emacsPackage}/bin/emacs -Q -nw -l ~/.config/emacs/init.el -batch -f batch-byte-compile ~/.config/emacs/init.el
-      rm -rfv ~/.config/emacs/eln-cache/
-      ${emacsPackage}/bin/emacs -Q -nw -l ~/.config/emacs/init.el -batch -f batch-native-compile ~/.config/emacs/init.el
-    '';
+let
+  prepare = ''
+    export HOME="$(pwd)"
+
+    mkdir -p ~/.config/emacs/
+    cp ${./init.el} ~/.config/emacs/init.el
+
+    mkdir -p ~/roam
+
+    export PATH=${pkgs.git}/bin:${pkgs.agda}/bin:"$PATH"
+    export EMACSLOADPATH=${pkgs.mu}/share/emacs/site-lisp/mu4e:
+    export EMACSNATIVELOADPATH=${pkgs.mu}/share/emacs/native-lisp:
+  '';
+  elc-config = pkgs.runCommand "emacs-elc-config" { } ''
+    ${prepare}
+
+    ${emacsPackage}/bin/emacs -Q \
+      --load ~/.config/emacs/init.el \
+      --batch \
+      -f 'batch-byte-compile' ~/.config/emacs/init.el
+
+    cp ./.config/emacs/init.elc "$out"
+  '';
+  eln-config = pkgs.runCommand "emacs-eln-config" { } ''
+    ${prepare}
+
+    file="$(
+      ${emacsPackage}/bin/emacs -Q \
+        --load ~/.config/emacs/init.el \
+        --batch \
+        --eval "(add-to-list 'native-comp-eln-load-path \"$HOME\")" \
+        --eval '(princ (comp-el-to-eln-filename "~/.config/emacs/init.el"))' \
+        -f 'batch-native-compile' ~/.config/emacs/init.el
+    )"
+
+    cp "$file" "$out"
+  '';
+in {
+  home.file = {
+    ".config/emacs/init.el".source = ./init.el;
+    ".config/emacs/init.elc" = {
+      source = elc-config;
+      onChange = ''
+        rm -rf ~/.config/emacs/eln-cache
+        dest="$(
+          ${emacsPackage}/bin/emacs -Q \
+            --batch \
+            --eval '(princ (comp-el-to-eln-filename "~/.config/emacs/init.el"))'
+        )"
+        mkdir -p "$(dirname "$dest")"
+        ln -s ${eln-config} "$dest"
+      '';
+    };
   };
 }
