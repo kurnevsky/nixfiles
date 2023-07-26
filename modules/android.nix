ea91ba475257454b332748281a6d7022f5ef0ae0
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-07-26 08:28:06 +0300

    Use patched gpg on android.

--- modules/android.nix
+++ modules/android.nix
@@ -87,6 +87,7 @@ in {
             pinentryFlavor = "curses";
           };
           programs = {
+            gpg.package = patchedPkgs.gnupg_patched;
             bash.initExtra = ''
               # Set cursor type to steady bar
               echo -e -n "\x1b[\x36 q"
