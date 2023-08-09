efa9cdc30ebb985b8f2c5ac0bc00f82ef75be346
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-08-09 13:24:22 +0300

    Improve filtering.

--- modules/emacs/early-init.el
+++ modules/emacs/early-init.el
@@ -24,35 +24,52 @@
   '(lambda (orig &rest args)
      (let ((name (plist-get args :name))
             (command (mapconcat #'identity (plist-get args :command) " "))
+            (backtrace)
             (allow))
        (mapbacktrace
          (lambda (_evald func _args _flags)
+           (when (symbolp func)
+             (push func backtrace))
            (setq allow (or allow
                          (eq func 'ispell-start-process)
-                         (eq func 'flycheck-start-command-checker)))))
-       (if (or allow (yes-or-no-p (format "Name: %.512s\nCommand: %.512s\nAllow `make-process' call?" name command)))
+                         (eq func 'flycheck-start-command-checker)
+                         (eq func 'rg-run)
+                         (eq func 'mu4e--server-start)
+                         (eq func 'mu4e--update-mail-and-index-real)))))
+       (setq backtrace (butlast backtrace))
+       (if (or allow (yes-or-no-p (format "Name: %.512s\nCommand: %.512s\nBacktrace: %.512S\nAllow `make-process' call?" name command backtrace)))
          (apply orig args)
          (signal 'error nil)))))
 
 (sec-wrap-function 'make-serial-process
   '(lambda (orig &rest args)
      (let ((name (plist-get args :name))
-            (port (plist-get args :port)))
-       (if (yes-or-no-p (format "Name: %.512s\nPort: %.512s\nAllow `make-serial-process' call?" name port))
+            (port (plist-get args :port))
+            (backtrace))
+       (mapbacktrace
+         (lambda (_evald func _args _flags)
+           (when (symbolp func)
+             (push func backtrace))))
+       (setq backtrace (butlast backtrace))
+       (if (yes-or-no-p (format "Name: %.512s\nPort: %.512s\nBacktrace: %.512S\nAllow `make-serial-process' call?" name port backtrace))
          (apply orig args)
          (signal 'error nil)))))
 
 (sec-wrap-function 'make-network-process
   '(lambda (orig &rest args)
      (let ((name (plist-get args :name))
+            (backtrace)
             (allow))
        (mapbacktrace
          (lambda (_evald func _args _flags)
+           (when (symbolp func)
+             (push func backtrace))
            (setq allow (or allow
                          (eq func 'server-start)
                          (eq func 'server-running-p)
                          (eq func 'server-eval-at)))))
-       (if (or allow (yes-or-no-p (format "Name: %.512s\nAllow `make-network-process' call?" name)))
+       (setq backtrace (butlast backtrace))
+       (if (or allow (yes-or-no-p (format "Name: %.512s\nBacktrace: %.512S\nAllow `make-network-process' call?" name backtrace)))
          (apply orig args)
          (signal 'error nil)))))
 ;;; early-init.el ends here
