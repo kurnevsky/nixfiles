bfa32736ff3389fde6c275fb385eb3570e0d15fb
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-08-28 12:31:50 +0300

    Activate zswap earlier.

--- modules/zswap.nix
+++ modules/zswap.nix
@@ -1,15 +1,14 @@
 { ... }:
 
 {
-  boot.kernelParams = [ "zswap.enabled=1" ];
-  boot.kernelModules = [ "lz4" "z3fold" ];
-  systemd.services.zswap-configure = {
-    description = "Configure zswap";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig.Type = "oneshot";
-    script = ''
-      echo lz4 > /sys/module/zswap/parameters/compressor
-      echo z3fold > /sys/module/zswap/parameters/zpool
-    '';
+  boot = {
+    kernelParams = [ "zswap.enabled=1" ];
+    initrd = {
+      kernelModules = [ "lz4" "z3fold" ];
+      preDeviceCommands = ''
+        echo lz4 > /sys/module/zswap/parameters/compressor
+        echo z3fold > /sys/module/zswap/parameters/zpool
+      '';
+    };
   };
 }
