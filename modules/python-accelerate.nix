696cb791984b4ce21831dba60391f7e63ff0fb71
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-10-23 14:19:03 +0300

    Bump diffusers.

--- modules/python-accelerate.nix
+++ modules/python-accelerate.nix
@@ -1,19 +1,19 @@
-{ python3Packages, fetchFromGitHub, pyyaml, packaging, numpy, pytorch, psutil
+{ python3Packages, fetchFromGitHub, pyyaml, packaging, numpy, torch, psutil
 , ... }:
 
 (python3Packages.buildPythonPackage rec {
   pname = "accelerate";
-  version = "0.15.0";
+  version = "0.23.0";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "huggingface";
     repo = "accelerate";
     rev = "v${version}";
-    hash = "sha256-agfbOaa+Nm10HZkd2Y7zR3R37n+vLNsxCyxZax6O3Lo=";
+    hash = "sha256-pFkEgE1NGLPBW1CeGU0RJr+1Nj/y58ZcljyOnJuR47A=";
   };
 
-  buildInputs = [ pyyaml packaging numpy pytorch ];
+  buildInputs = [ pyyaml packaging numpy torch ];
 
   propagatedBuildInputs = [ psutil ];
 
--- modules/python-diffusers.nix
+++ modules/python-diffusers.nix
@@ -1,21 +1,21 @@
-{ python3Packages, fetchFromGitHub, ftfy, scipy, pytorch, huggingface-hub, regex
+{ python3Packages, fetchFromGitHub, ftfy, scipy, torch, huggingface-hub, regex
 , importlib-metadata, ... }:
 
 python3Packages.buildPythonPackage rec {
   pname = "diffusers";
-  version = "0.14.0";
+  version = "0.21.4";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "huggingface";
     repo = "diffusers";
     rev = "refs/tags/v${version}";
-    hash = "sha256-2Su4TWsUwAyWxtIJwTAZf36u/k5HRM4dc0WQUyTPlTM=";
+    hash = "sha256-nyKZVnthADMFA2DVlCPLYJUf0R3fV++OzGhjHDJnMI0=";
   };
 
   propagatedBuildInputs = [ ftfy scipy ];
 
-  buildInputs = [ pytorch huggingface-hub regex importlib-metadata ];
+  buildInputs = [ torch huggingface-hub regex importlib-metadata ];
 
   # Many tests require internet access.
   doCheck = false;
