a905cdca2ef834b531e552b112a987308ed5237a
Evgeny Kurnevsky <kurnevsky@gmail.com>
2024-03-15 23:34:33 +0300

    Update iosevka.

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -340,7 +340,6 @@
             design = {
               capital-g = "toothed-serifless-hooked";
               capital-j = "serifed";
-              capital-l = "motion-serifed";
               capital-q = "crossing";
               u = "toothed-serifless";
               capital-u = "toothed-serifless";
