d54b16338cc696025dfc0ddec316038f213db291
Evgeny Kurnevsky <kurnevsky@gmail.com>
2024-03-24 21:19:56 +0300

    Update wesnoth.

--- modules/patches.nix
+++ modules/patches.nix
@@ -9,11 +9,21 @@ let
           "https://github.com/NixOS/nixpkgs/commit/453281114f968f1c355c14db82555c80b08ed568.diff";
         sha256 = "sha256-2TNCoB6WcaRGxw/HgPK0aWZh8Mt7dXu/cTyZOvFwyIM=";
       })
+      (pkgs.fetchpatch {
+        url =
+          "https://github.com/NixOS/nixpkgs/commit/61cb7385bd311a6f55813f123c960adab699c96c.diff";
+        sha256 = "sha256-8QchRS+IdXxHzJSvUgMMCnDwNVxmxPeyXlj/BvxAVoc=";
+      })
     ];
   };
   patchedPkgs = (import patchesDrv { system = "x86_64-linux"; });
 in {
   disabledModules = [ ];
   imports = [ ];
-  nixpkgs.overlays = [ (self: super: { iosevka = patchedPkgs.iosevka; }) ];
+  nixpkgs.overlays = [
+    (self: super: {
+      iosevka = patchedPkgs.iosevka;
+      wesnoth = patchedPkgs.wesnoth;
+    })
+  ];
 }
