06fb7fc0e94cf3d92463d688e18c64541e914935
Evgeny Kurnevsky <kurnevsky@gmail.com>
2024-04-07 13:14:38 +0300

    Bump.

--- modules/patches.nix
+++ modules/patches.nix
@@ -3,17 +3,11 @@
 let
   patchesDrv = pkgs.applyPatches {
     src = pkgs.path;
-    patches = [
-      (pkgs.fetchpatch {
-        url =
-          "https://github.com/NixOS/nixpkgs/commit/1691576fef01c9d5a7e0ee3eea04fe945f66c6ac.diff";
-        sha256 = "sha256-vPYCt8veKmh0eoFMXamtuCW1E1CKSyXGSDn3Ut6Nxcw=";
-      })
-    ];
+    patches = [ ];
   };
   patchedPkgs = import patchesDrv { system = "x86_64-linux"; };
 in {
   disabledModules = [ ];
   imports = [ ];
-  nixpkgs.overlays = [ (_self: _super: { inherit (patchedPkgs) iosevka; }) ];
+  nixpkgs.overlays = [ ];
 }
