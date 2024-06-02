a4eef7c361004a58831e42a8ab409cb75017c549
Evgeny Kurnevsky <kurnevsky@gmail.com>
2024-06-02 10:55:17 +0300

    Reduce duplication.

--- modules/common.nix
+++ modules/common.nix
@@ -57,17 +57,7 @@ in {
     };
   };
 
-  networking.hosts = {
-    "192.168.14.2" = [ "dell" ];
-    "192.168.14.3" = [ "evo" ];
-    "192.168.14.4" = [ "acer" ];
-    "192.168.14.5" = [ "pc" ];
-    "200:3b5c:7981:d112:f1e2:7b3f:218f:e7ea" = [ "dell-ygg" ];
-    "200:d196:9226:6955:4190:dc33:bac7:17c7" = [ "evo-ygg" ];
-    "201:613a:44c6:38ad:a0f2:d452:dd0a:94c7" = [ "acer-ygg" ];
-    "201:b5cd:a295:d34c:a75f:b369:6002:c676" = [ "pc-ygg" ];
-    "200:6381:a789:fbbe:3411:2135:e3b:b4a9" = [ "digitalocean-ygg" ];
-  };
+  networking.hosts = import ./hosts.nix;
 
   programs = {
     bash.interactiveShellInit = ''
