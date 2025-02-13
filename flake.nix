877232d753d0bc6700d78b5f6825d388007e6586
Evgeny Kurnevsky <kurnevsky@gmail.com>
2025-02-13 16:37:52 +0100

    Remove fixed overrides.

--- flake.nix
+++ flake.nix
@@ -7,13 +7,6 @@
       ref = "nixos-unstable";
     };
 
-    nixpkgs-old = {
-      type = "github";
-      owner = "NixOS";
-      repo = "nixpkgs";
-      ref = "3566ab7246670a43abd2ffa913cc62dad9cdf7d5";
-    };
-
     fenix = {
       type = "github";
       owner = "nix-community";
@@ -143,26 +136,6 @@
         ]) common-home)
         # Keep flake inputs from being garbage collected
         { system.extraDependencies = collectFlakeInputs inputs.self; }
-        (
-          # TODO: a lot of things are broken :(
-          { pkgs, ... }:
-          let
-            oldPkgs = import inputs.nixpkgs-old {
-              inherit (pkgs.stdenv.targetPlatform) system;
-            };
-          in
-          {
-            nixpkgs.overlays = [
-              (_self: _super: {
-                inherit (oldPkgs)
-                  rocmPackages
-                  cataclysm-dda
-                  dsniff
-                  ;
-              })
-            ];
-          }
-        )
       ];
       desktopModules = commonModules ++ [
         {
@@ -225,8 +198,6 @@
           environment.systemPackages = [
             (llamaOverride pkgs config (
               inputs.llama-cpp.packages.${pkgs.system}.rocm.override {
-                # TODO: rocm is broken in nixpkgs
-                inherit (pkgs) rocmPackages;
                 rocmGpuTargets = gpuTargets;
               }
             ))
