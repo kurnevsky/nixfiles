c7cad87efd49bb0c16cbebae8b07b4ac5131f867
Evgeny Kurnevsky <kurnevsky@gmail.com>
2025-05-27 16:36:15 +0200

    Convert chromium extensions into json.

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -87,53 +87,9 @@
             super.lib.versions.major super.ungoogled-chromium.version
           );
         in
-        {
-          dark-reader = createChromiumExtension {
-            id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";
-            sha256 = "sha256:1lc5ny57rdpqzx07gwj44pl3m9mg9nq6p5kjchrmjjh4hkk5wsny";
-            version = "4.9.106";
-          };
-          decentraleyes = createChromiumExtension {
-            id = "ldpochfccmkkmhdbclfhpagapcfdljkj";
-            sha256 = "sha256:198k1hyzf3a1yz4chnx095rwqa15hkcck4ir6xs6ps29qgqw8ili";
-            version = "3.0.0";
-          };
-          plasma-integration = createChromiumExtension {
-            id = "cimiefiiaegbelhefglklhhakcgmhkai";
-            sha256 = "sha256:0nix7ilqdk12fx5afqn1dswq9ana096d440zr7p8y9ijvjgd9vlv";
-            version = "2.0";
-          };
-          privacy-badger = createChromiumExtension {
-            id = "pkehgijcmpdhfbdbbnkijodmdjhbjlgp";
-            sha256 = "sha256:0kx6pgk3m9mdqwnh2lxw1cid9csv6xvchdw55svimrl5ky2dqlzx";
-            version = "2025.5.14";
-          };
-          sponsorblock = createChromiumExtension {
-            id = "mnjggcdmjocbbbhaepdhchncahnbgone";
-            sha256 = "sha256:1vs5bqrkqkssmn32iblp4xr5pqbh941rc0zl8qmyz7khhp318aw1";
-            version = "5.12.2";
-          };
-          ublock-origin-lite = createChromiumExtension {
-            id = "ddkjiahejlhfcafbddmgiahcphecmpfh";
-            sha256 = "sha256:0ynlq33134ajppfhb9hx9cqsxqqm4l492fnrg3ssq91m0sjqvzfa";
-            version = "2025.512.1008";
-          };
-          find-plus = createChromiumExtension {
-            id = "fddffkdncgkkdjobemgbpojjeffmmofb";
-            sha256 = "sha256:03yhzc8yhcx2shm34pm54fhrnhb3hfccgnk2q6qkhysk9ylpywq3";
-            version = "2.2.2";
-          };
-          i-still-dont-care-about-cookies = createChromiumExtension {
-            id = "edibdbjcniadpccecjdfdjjppcpchdlm";
-            sha256 = "sha256:11k7cxcjafs8ziaxl4bilbfwbgl2yf1p6v1bvwszadcr14xyvgsj";
-            version = "1.1.4";
-          };
-          refined-github = createChromiumExtension {
-            id = "hlepfoohegkhhmjieoechaddaejaokhf";
-            sha256 = "sha256:08chhqp83xdzhv5fd6xvgh61k17srfij3yj2hm6851f2qvria8zw";
-            version = "25.4.8";
-          };
-        };
+        super.lib.mapAttrs (_: createChromiumExtension) (
+          builtins.fromJSON (builtins.readFile ./chromium-extensions.json)
+        );
     })
   ];
 }
