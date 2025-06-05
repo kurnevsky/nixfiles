edb0fe11e6abe4fb889855cc2418e88ed94dac31
Evgeny Kurnevsky <kurnevsky@gmail.com>
2025-06-05 14:01:07 +0200

    Specify jobslots.

--- modules/nerd-font-patch.nix
+++ modules/nerd-font-patch.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation {
   src = font;
   buildPhase = ''
     find \( -name \*.ttf -o -name \*.otf \) -print0 | \
-      ${parallel}/bin/parallel -u -0 ${nerd-font-patcher}/bin/nerd-font-patcher -c {}
+      ${parallel}/bin/parallel -j ''${NIX_BUILD_CORES:0} -u -0 ${nerd-font-patcher}/bin/nerd-font-patcher -c {}
   '';
   installPhase = "cp -a . $out";
 }
