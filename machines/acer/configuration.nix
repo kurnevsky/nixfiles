590c043dc994b3503753a94049005ac51b062c3a
Evgeny Kurnevsky <kurnevsky@gmail.com>
2025-07-17 11:51:47 +0300

    Configure locales.

--- machines/acer/configuration.nix
+++ machines/acer/configuration.nix
@@ -47,6 +47,15 @@
     keyMap = "ru";
   };
 
+  i18n = {
+    supportedLocales = [
+      "C.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+      "ru_RU.UTF-8/UTF-8"
+    ];
+    defaultLocale = "ru_RU.UTF-8";
+  };
+
   time.timeZone = "Europe/Minsk";
 
   environment.systemPackages = with pkgs; [
