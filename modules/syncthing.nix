c1ff3c5ca8f3e018bcda8e2200e72193a6c6a9dd
Evgeny Kurnevsky <kurnevsky@gmail.com>
2025-07-30 11:13:40 +0200

    Add android.

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -19,6 +19,7 @@
       devices = lib.filterAttrs (name: _: name != config.networking.hostName) {
         evo.id = "56323HF-7SEVXR2-34IUVUU-PYQYRZ6-OCBF66D-BKWC5V2-W236TTT-57YEIAR";
         pc.id = "ZW66CUX-VYZDALZ-QD4EVPD-2PZ6NMV-G6R7C7J-SRMLFMU-ZOMMXJU-HCZV4AZ";
+        android.id = "PPGMSUR-ZZLBZ5W-DC6KZAB-J5EZZIC-QG6TDNP-K24E3X2-IVIZWYX-SIUT2QG";
         digitalocean = {
           addresses = [ "quic://kropki.org" ];
           id = "IIJ6Y4L-VIWHB67-VR74H75-WNFP4G3-5CUY6GD-O6SCUUP-2HWQODL-BBS3CA3";
