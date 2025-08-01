c17a268799cc36b2c8bbcfd325e2456320969f78
Evgeny Kurnevsky <kurnevsky@gmail.com>
2025-08-01 10:47:03 +0200

    Update network.

--- machines/digitalocean/networking.nix
+++ machines/digitalocean/networking.nix
@@ -3,50 +3,69 @@
   # This file was populated at runtime with the networking
   # details gathered from the active system.
   networking = {
-    nameservers = [ "8.8.8.8" ];
-    defaultGateway = "82.196.15.1";
-    defaultGateway6 = "2a03:b0c0:0:1010::1";
+    nameservers = [
+      "8.8.8.8"
+    ];
+    defaultGateway = "167.71.48.1";
+    defaultGateway6 = {
+      address = "2a03:b0c0:3:f0::1";
+      interface = "eth0";
+    };
     dhcpcd.enable = false;
     usePredictableInterfaceNames = lib.mkForce false;
     interfaces = {
       eth0 = {
         ipv4.addresses = [
           {
-            address = "82.196.15.215";
-            prefixLength = 24;
+            address = "167.71.63.75";
+            prefixLength = 20;
           }
           {
-            address = "10.14.0.5";
+            address = "10.19.0.5";
             prefixLength = 16;
           }
         ];
         ipv6.addresses = [
           {
-            address = "2a03:b0c0:0:1010::4c:5001";
+            address = "2a03:b0c0:3:f0:0:1:2270:f000";
             prefixLength = 64;
           }
           {
-            address = "fe80::4c13:81ff:fe05:22f8";
+            address = "fe80::5c86:feff:fe57:9484";
             prefixLength = 64;
           }
         ];
         ipv4.routes = [
           {
-            address = "82.196.15.1";
+            address = "167.71.48.1";
             prefixLength = 32;
           }
         ];
         ipv6.routes = [
           {
-            address = "2a03:b0c0:0:1010::1";
+            address = "2a03:b0c0:3:f0::1";
             prefixLength = 128;
+          }
+        ];
+      };
+      eth1 = {
+        ipv4.addresses = [
+          {
+            address = "10.114.0.2";
+            prefixLength = 20;
+          }
+        ];
+        ipv6.addresses = [
+          {
+            address = "fe80::5803:50ff:fec4:6c60";
+            prefixLength = 64;
           }
         ];
       };
     };
   };
   services.udev.extraRules = ''
-    ATTR{address}=="4e:13:81:05:22:f8", NAME="eth0"
-    ATTR{address}=="aa:4a:32:9c:cd:b0", NAME="eth1"
+    ATTR{address}=="5e:86:fe:57:94:84", NAME="eth0"
+    ATTR{address}=="5a:03:50:c4:6c:60", NAME="eth1"
   '';
 }
