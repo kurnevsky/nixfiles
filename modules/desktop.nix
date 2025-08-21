7127d061bffecb6e30415eedeb0454fd0e57873a
Evgeny Kurnevsky <kurnevsky@gmail.com>
2025-08-21 09:48:34 +0200

    Fix calendar sync.

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1002,6 +1002,7 @@
                       "pass"
                       "evo/outlook-ical"
                     ];
+                    userAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:141.0) Gecko/20100101 Firefox/141.0";
                   };
                   remote.type = "http";
                   local = {
