4f9143d15c5940d19e3f98ed0ece5679ca5bff23
Evgeny Kurnevsky <kurnevsky@gmail.com>
2025-09-01 16:26:05 +0200

    Configure network.

--- machines/vps/configuration.nix
+++ machines/vps/configuration.nix
@@ -7,6 +7,22 @@
     tmp.cleanOnBoot = true;
   };
 
+  networking.useDHCP = false;
+
+  systemd.network = {
+    enable = true;
+    networks."10-lan" = {
+      matchConfig.Name = "enp1s0";
+      networkConfig = {
+        DHCP = "ipv4";
+        IPv6AcceptRA = true;
+        Address = [ "2a01:4f8:c013:71a5::1/64" ];
+        Gateway = [ "fe80::1" ];
+      };
+      linkConfig.RequiredForOnline = "routable";
+    };
+  };
+
   system.stateVersion = "25.05";
 
   home-manager.users = {
