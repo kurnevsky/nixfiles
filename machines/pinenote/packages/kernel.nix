d0dcc1fc954717ed9a137652114a62388a7ea0c7
Evgeny Kurnevsky <kurnevsky@gmail.com>
2025-09-11 22:06:26 +0200

    Fix kernel build.

--- machines/pinenote/packages/kernel.nix
+++ machines/pinenote/packages/kernel.nix
@@ -1,27 +1,39 @@
 { pkgs, fetchFromSourcehut, ... }:
 
 pkgs.linuxPackagesFor (
-  (pkgs.linuxKernel.kernels.linux_6_15.override {
-    argsOverride = rec {
-      src = fetchFromSourcehut {
-        owner = "~hrdl";
-        repo = "linux";
-        rev = "f75fe16d81ae784b8cd2b915113f3a99ff812777";
-        sha256 = "sha256-DhMiZMcwownJJRqIYOj87E/j34jJZb2/rTOhYuMumG4=";
+  (
+    let
+      kernelPatches = pkgs.callPackage "${pkgs.path}/pkgs/os-specific/linux/kernel/patches.nix" { };
+      kernel = pkgs.callPackage "${pkgs.path}/pkgs/os-specific/linux/kernel/mainline.nix" {
+        branch = "6.15";
+        kernelPatches = [
+          kernelPatches.bridge_stp_helper
+          kernelPatches.request_key_helper
+        ];
       };
-      version = "6.15.0-rc3";
-      modDirVersion = version;
-      defconfig = "pinenote_defconfig";
+    in
+    kernel.override {
+      argsOverride = rec {
+        src = fetchFromSourcehut {
+          owner = "~hrdl";
+          repo = "linux";
+          rev = "f75fe16d81ae784b8cd2b915113f3a99ff812777";
+          sha256 = "sha256-DhMiZMcwownJJRqIYOj87E/j34jJZb2/rTOhYuMumG4=";
+        };
+        version = "6.15.0-rc3";
+        modDirVersion = version;
+        defconfig = "pinenote_defconfig";
 
-      # These are required to make the build work:
-      extraConfig = ''
-        VIDEO_THP7312 n
-        CRYPTO_AEGIS128_SIMD n
-        ROCKCHIP_DW_HDMI_QP n
-      '';
-      ignoreConfigErrors = true;
-    };
-  }).overrideAttrs
+        # These are required to make the build work:
+        extraConfig = ''
+          VIDEO_THP7312 n
+          CRYPTO_AEGIS128_SIMD n
+          ROCKCHIP_DW_HDMI_QP n
+        '';
+        ignoreConfigErrors = true;
+      };
+    }
+  ).overrideAttrs
     (old: {
       postInstall = ''
         cp "$out/dtbs/rockchip/rk3566-pinenote-v1.2.dtb" "$out/dtbs/rockchip/pn.dtb"
