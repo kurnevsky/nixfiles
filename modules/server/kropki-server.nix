70b19c4737920717f618fa3e59c06b0f4ce295dd
Evgeny Kurnevsky <kurnevsky@gmail.com>
2026-01-25 13:12:01 +0100

    Bump.

--- modules/server/kropki-server.nix
+++ modules/server/kropki-server.nix
@@ -11,8 +11,8 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "pointsgame";
     repo = "oppai-rs";
-    rev = "0807929c71405392caf755445bd2757a4993727a";
-    hash = "sha256-A18a86Pftw86PGMLBFfMZSHPZqUCU3sguGpLwXeV8VQ=";
+    rev = "cc7b6c5ed9fbc25faf12a59d79b7295ba798eb19";
+    hash = "sha256-Bl91WqU15HRPNfDBfKKpFiwr7bX6qD5F/W8XDMVS8c4=";
   };
 
   buildAndTestSubdir = "server";
