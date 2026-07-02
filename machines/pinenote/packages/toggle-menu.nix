fc3d6c0abbce47719f3e64d6623926a98e57d8ad
Evgeny Kurnevsky <kurnevsky@gmail.com>
2026-07-02 14:43:59 +0200

    Fix warning.

--- machines/pinenote/packages/toggle-menu.nix
+++ machines/pinenote/packages/toggle-menu.nix
@@ -4,5 +4,5 @@
 }:
 
 pkgs.writeShellScriptBin "toggle_menu.sh" ''
-  ${pkgs.nwg-menu}/bin/nwg-menu -fm ${pkgs.xfce.thunar}/bin/thunar -term ${pkgs.alacritty}/bin/alacritty -va top -isl 64 -iss 64 -s menu-start.css &
+  ${pkgs.nwg-menu}/bin/nwg-menu -fm ${pkgs.thunar}/bin/thunar -term ${pkgs.alacritty}/bin/alacritty -va top -isl 64 -iss 64 -s menu-start.css &
 ''
