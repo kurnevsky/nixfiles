0762d6ebc475854d844f88929abf8f1d811887bc
Evgeny Kurnevsky <kurnevsky@gmail.com>
2026-08-12 15:26:53 +0200

    Remove tram-rpc.
    It still can't load properly.

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -23,7 +23,6 @@ lib.mkMerge [
 
     environment.systemPackages = with pkgs; [
       emacs-lsp-booster
-      (callPackage ./emacs-tramp-rpc-server.nix { })
     ];
   }
   (import ./for-all-home-users.nix users (import ./emacs/home.nix emacs))
